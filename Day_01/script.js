bb4470d7150beb843a143aed43981157753dc9c4
iftekhar395 <iftekharhossain395@gmail.com>
2025-02-25 23:51:00 +0600

    testing document.write()

--- Day_01/script.js
+++ Day_01/script.js
@@ -1,2 +1,3 @@
 console.log("Hello World!!!");
-document.getElementById("setText").innerText = "Here is my div :)";
+//document.getElementById("setText").innerText = "Here is my div :)";
+document.write("Some Additional Text")
