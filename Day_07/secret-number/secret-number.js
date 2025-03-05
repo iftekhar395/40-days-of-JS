11bb3982be4673167fb1e44cff064fb42dbaea66
iftekhar395 <iftekharhossain395@gmail.com>
2025-03-05 20:42:26 +0600

    Project Issue solved

--- Day_07/secret-number/secret-number.js
+++ Day_07/secret-number/secret-number.js
@@ -2,19 +2,33 @@
 function startSecretNumberGame(){
     //Get the system 
     const sysNumber = Math.floor(Math.random()*10)+1;
-    let attemptCount = 0;
-    let userInput = Number(prompt("Guess a number between 1 and 10"));
-    if(userInput !== null){
-        console.log(sysNumber);
-        while(userInput !== sysNumber){
-            attemptCount += 1
-            reTry(userInput, sysNumber);
-            //Guess until it's not matched
-            userInput = Number(prompt("Guess a number between 1 and 10"));
-            console.log(typeof userInput, typeof sysNumber)
-        }
+    let attemptCount = 1;
+    let userInput, userInputtoNum;
 
-        console.log(`Congrats!!! You have guessed the number ${attemptCount}times. The number was ${sysNumber}`);
+    function reTry(){
+        userInput = prompt("Guess a number between 1 and 10");
+        userInputtoNum = Number(userInput);
+        if(userInput === ''){
+            reTry();
+        }else if(userInput !== null){//if not cancel
+            //console.log(sysNumber);
+            if(userInputtoNum !== sysNumber){
+                attemptCount += 1;
+                if(userInputtoNum>sysNumber){
+                    console.log("Your guess is higher. Try a smaller number.");
+                }else{
+                    console.log("Your guess is lower. Try a bigger number.");
+                }
+                //Guess until it's not matched
+                reTry();
+            }
+        }else{
+            console.log("You have exited from the game");
+        }
+    }
+    reTry();
+    if(userInputtoNum === sysNumber){
+        console.log(`Congrats!!! You have guessed the number ${attemptCount}time(s). The number was ${sysNumber}`);
         //To play again
         const playAgainPrompt = prompt("Do You Want to Play Again? (yes/no)")
         const playAgain = playAgainPrompt ? playAgainPrompt.toLowerCase() : "no";
@@ -23,18 +37,7 @@ function startSecretNumberGame(){
         } else {
             console.log("Thanks for Playing! See you Next Time")
         }
-    }else{
-        console.log("You have exited from the game");
     }
 }
 
-function reTry(userInput,sysNumber){
-    if(userInput>sysNumber){
-        console.log("Your guess is higher. Try a smaller number.");
-    }else{
-        console.log("Your guess is lower. Try a bigger number.");
-    }
-}
-
-//Start the game
 startSecretNumberGame();
