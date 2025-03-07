0b57dfba627c553b201bd9ba86f1d41881e6f516
iftekhar395 <iftekharhossain395@gmail.com>
2025-03-07 06:04:58 +0600

    Day 8 done

---
+++ Day_08/README.md
@@ -0,0 +1,11 @@
+# GEC and FEC with CP and EP flow
+
+![alt text](<Exicution Context.drawio.png>)
+
+# Stack and Heap Flow
+
+![alt text](<Stack & Heap Flow.drawio(3).png>)
+
+# Stack Diagram
+
+![alt text](Stack.drawio(4).png)
---
+++ Day_08/index.js
@@ -0,0 +1,23 @@
+/*
+Execution Context
+    Global Execution Context
+        Creation Phase:
+            1. window object
+            2. this keyword
+            3. window===this
+
+                var name = 'Tom'
+                function sayName(){
+                    console.log(this.name)
+                }
+
+            4. allocate memory for variable 'name' and function 'sayName()'
+            5. name will be initialized by undefined
+            6. function body will be placed directly into memory
+
+        Execution Phase:
+            1. assign the value 'Tom' to the variable name
+
+
+
+*/
